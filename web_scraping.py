bd9876f8011101a2c2973c71689c9c04e1da8c6c
Lourenço Costa <24901330+costa86@users.noreply.github.com>
2019-04-23 17:26:52 -0300

    Add files via upload

---
+++ web_scraping.py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
+import time, requests
+import pandas as pd
+from bs4 import BeautifulSoup as bs
+headers = {
+    'user-agent': 'Lourenço Costa',
+    'from': 'lourenzo9538@gmail.com'
+}
+
